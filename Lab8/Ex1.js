0b3ae76577a85d445ea4b7eba072da124a9fba99
myago13 <54603677+myago13@users.noreply.github.com>
2019-10-06 20:39:34 -1000

    Create Ex1.js

---
+++ Lab8/Ex1.js
@@ -0,0 +1,15 @@
+var age= 20;
+var number=50;
+
+for(var number=50; number > age; number--){
+  console.log(`Are you  ${number} years old?`);
+
+}
+console.log(`You must be ${number} years old`);
+
+/*var age= 20;
+for(var number = 1; number<age; number++){
+console.log(`Are you ${number} years old?`);
+
+}
+console.log(`You must be ${number} years old`);*/
