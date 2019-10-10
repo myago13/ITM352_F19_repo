c0cf7f3550fdaf9e484b5996e22f8bfa2ed241eb
myago13 <54603677+myago13@users.noreply.github.com>
2019-10-10 10:40:58 -1000

    Update Ex1 copy.js

--- Lab8/Ex1 copy.js
+++ Lab8/Ex1 copy.js
@@ -5,3 +5,18 @@ for(var number=1; number<age; number++){
 
 }
 console.log(`are you${number} years old?`);
+
+
+//var age= 20;
+var number=0;
+while(number <age){
+    
+    number++;
+
+if(number > (age/2) ){
+    console.log("Don't ask how old I am!");
+  process.exit();
+}
+console.log(`Are you ${number} years old?`);
+}
+console.log(`You must be ${number} years old`);
