fa176474ad1bca69d25f0a7479f97b3c3dfa1d2b
myago13 <54603677+myago13@users.noreply.github.com>
2019-10-21 20:07:10 -1000

    Update Ex1.js

--- Lab8/Ex1.js
+++ Lab8/Ex1.js
@@ -1,4 +1,4 @@
-var age= 20;
+/*var age= 20;
 var number=50;
 
 for(var number=50; number > age; number--){
@@ -13,3 +13,15 @@ console.log(`Are you ${number} years old?`);
 
 }
 console.log(`You must be ${number} years old`);*/
+
+product1 = { 'name': 'small gumball', 'price': 0.02 };
+product2 = { 'name': 'medium gumball', 'price': 0.05 };
+product3 = { 'name': 'large gumball', 'price': 0.07 };
+
+// array of all products
+products = [product1, product2, product3];
+cart_quantities = [2,0,4]; // corresponds to products array
+for(i=0; i<products.length; i++) {
+console.log(`extended price for product ${products[i].name} is ${products[i].price*cart_quantities[i]}`);
+
+}
