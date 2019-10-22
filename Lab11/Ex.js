f1aaa996b410807c058217424899095e10c6aaa0
myago13 <54603677+myago13@users.noreply.github.com>
2019-10-21 20:07:12 -1000

    Create Ex.js

---
+++ Lab11/Ex.js
@@ -0,0 +1,19 @@
+
+attributes  =  "Melissa;20;20.5;-19.5" ;
+theSeparator = ';'; 
+parts = attributes.split(theSeparator);
+
+parts= ['Melissa',20,20.5,-19.5];
+
+for(i=0; i< parts.length; i++){
+console.log(typeof parts[i]);
+
+}
+console.log(parts.join(theSeparator));
+
+
+fucntion isNonNegInt(q) {
+console.log('hey!');
+    
+}
+isNonNegInt();
