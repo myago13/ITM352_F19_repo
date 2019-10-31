37f07e9b49824b94066f190b3354dded34bdb630
myago13 <54603677+myago13@users.noreply.github.com>
2019-10-31 11:40:39 -1000

    Update Ex.js

--- Lab11/Ex.js
+++ Lab11/Ex.js
@@ -3,17 +3,27 @@ attributes  =  "Melissa;20;20.5;-19.5" ;
 theSeparator = ';'; 
 parts = attributes.split(theSeparator);
 
-parts= ['Melissa',20,20.5,-19.5];
+//parts= ['Melissa',20,20.5,-19.5];
 
-for(i=0; i< parts.length; i++){
-console.log(typeof parts[i]);
+//for(i=0; i< parts.length; i++){
+parts.forEach(printIT);
+
+//}
+function printIT(item, index) {
+console.log( (typeof item == 'string' && item.length > 0)?true:false) 
+   
 
 }
 console.log(parts.join(theSeparator));
+function isNonNegInt (q,returnErrors =false) {
 
-
-fucntion isNonNegInt(q) {
-console.log('hey!');
+errors = []; // assume no errors at first
+if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+return returnErrors ? errors : (errors.length == 0) 
     
-}
-isNonNegInt();
+};
+//console.log(isNonNegInt(-2));
+
+//console.log('hey! '+ q);
