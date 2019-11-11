13d0adcc7669ab3964b7c43f5cfc17d9f5ff79a6
myago13 <54603677+myago13@users.noreply.github.com>
2019-11-10 19:32:14 -1000

    Create Cookie_data.js

---
+++ Yago_Melissa_Assignment1/Cookie_data.js
@@ -0,0 +1,63 @@
+<script>
+//Product 1
+var product1 = {
+Cookie: "Choclate Chip",
+Price: 5.00,
+Description: "Contains five delicious chocolate chip cookies",
+image: "https://www.handletheheat.com/wp-content/uploads/2018/02/BAKERY-STYLE-CHOCOLATE-CHIP-COOKIES-9.jpg"
+};
+
+
+// Product 2
+var product2= {
+Cookie: "Oatmeal Raisin",
+Price: 4.00,
+Description: "Contains five delicious oatmeal raisin cookies",
+image: "https://www.twopeasandtheirpod.com/wp-content/uploads/2017/11/Oatmeal-Raisin-Cookies-2.jpg"
+};
+
+
+// Product 3
+var product3= {
+Cookie: "Peanut Butter",
+Price: 4.50,
+Description: "Contains five delicious peanut butter cookies",
+image: "https://preppykitchen.com/wp-content/uploads/2019/06/Peanut-Butter-Cookie-Feature.jpg"
+};
+
+
+// Product 4
+var product4= {
+Cookie: "Almond",
+Price: 5.50,
+Description: "Contains five almond cookies",
+image: "https://pixelbaking.files.wordpress.com/2014/01/dsc06590-e1390882341114.jpg"
+};
+
+
+// Product 5
+var product5 = {
+Cookie: "Custom Made",
+Price: 8.00,
+Description: "Contains five sugar cookies with a design of your choice. Please email your image to Cookies808@gmail.com along with your order number",
+image: "https://cdn.shopify.com/s/files/1/0014/7695/2110/files/pyo_cookies_large.png?v=1561793010"
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</script>
