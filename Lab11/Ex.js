4253aa57591efd08a8ab565b91738815b39c74f8
myago13 <54603677+myago13@users.noreply.github.com>
2019-11-10 19:31:44 -1000

    Update Ex.js

--- Lab11/Ex.js
+++ Lab11/Ex.js
@@ -15,15 +15,15 @@ console.log( (typeof item == 'string' && item.length > 0)?true:false)
 
 }
 console.log(parts.join(theSeparator));
-function isNonNegInt (q,returnErrors =false) {
 
+function isNonNegInt (q,returnErrors =false) {
 errors = []; // assume no errors at first
 if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
 if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
 if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-return returnErrors ? errors : (errors.length == 0) 
+return returnErrors ? errors : (errors.length == 0); 
     
-};
+}
 //console.log(isNonNegInt(-2));
 
 //console.log('hey! '+ q);
