b495fcf7a7a726e4c26ce3518962bcebd586e741
myago13 <54603677+myago13@users.noreply.github.com>
2019-11-10 19:32:03 -1000

    Create info_server_Ex3.js

---
+++ Lab13/info_server_Ex3.js
@@ -0,0 +1,45 @@
+var products = require('./product_data')
+var express = require('express');
+var myParser = require("body-parser");
+var app = express();
+var fs = require('fs'); 
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to ' + request.path);
+   next();
+  
+});
+app.use(myParser.urlencoded({ extended: true }));
+app.postprocess_quantity_form(request.body, response); {
+   let POST = request.body;
+  
+ function process_quantity_form (POST, response) { 
+   console.log(request.query);
+   if (typeof POST['quantity_textbox'] != 'undefined'){
+    displayPurchase(POST, response);
+   
+}
+};
+
+ app.use(express.static('./public'));
+app.listen(8080, () => console.log(`listening on port 8080`));
+
+function isNonNegInt (q,returnErrors =false) {
+    errors = []; // assume no errors at first
+    if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+    if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+    if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+    return returnErrors ? errors : (errors.length == 0); 
+        
+    }
+
+    function displayPurchase(POST, response){
+        q = POST['quantity_textbox.value']
+        if (typeof POST['quantity_textbox'] != 'undefined') {
+            q = POST['quantity_textbox'];
+            if (isNonNegInt(q)) {
+                var contents = fs.readFileSync('./Views/display_quanity_template.view', 'utf8');
+                response.send(eval('`' + contents + '`')); // render template string
+            } else {
+                response.send(`${q} is not a quantity!`);
+            }}}
+            } 
