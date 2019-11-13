b387941788f6568ab75b398e6b81fce0f208db21
myago13 <54603677+myago13@users.noreply.github.com>
2019-11-13 09:50:07 -1000

    Create cookie_data.js

---
+++ Yago_Melissa_Assignment1/public/cookie_data.js
@@ -0,0 +1,56 @@
+//json array that contains all five products with their respective price, description and image. Above is a variable called products that defines the array that is used in the invoice & products_display page. 
+//Code was learned through instructions given on Assignment 1
+
+var products =
+[
+{
+"Cookie": "Chocolate Chip",
+"Price": 5.00,
+"Description": "Contains five delicious chocolate chip cookies",
+"image": "./images/chocolatechip.jpg"
+},
+
+
+{
+"Cookie": "Oatmeal Raisin",
+"Price": 4.00,
+"Description": "Contains five delicious oatmeal raisin cookies",
+"image": "./images/oatmeal.jpg"
+},
+
+{
+"Cookie": "Peanut Butter",
+"Price": 4.50,
+"Description": "Contains five delicious peanut butter cookies",
+"image": "./images/peanutbutter.jpg"
+},
+
+
+{
+"Cookie": "Almond",
+"Price": 5.50,
+"Description": "Contains five almond cookies",
+"image": "./images/almond.jpg"
+},
+
+
+{
+"Cookie": "Custom Made",
+"Price": 8.00,
+"Description": "Contains five sugar cookies with a design of your choice. Please email your image to Cookies808@gmail.com along with your order number",
+"image": "./images/custom.png"
+}];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
