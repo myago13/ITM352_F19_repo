7d4c48a33e1fe52cac97037aecfcd208cd90714e
myago13 <54603677+myago13@users.noreply.github.com>
2019-12-01 12:37:50 -1000

    Create product.js

---
+++ Assignment_2.1 copy/public/product.js
@@ -0,0 +1,36 @@
+  
+var products = [
+    { 
+     "flower" : "Catharanthus",
+     "price" : 4.00,
+     "image" : "./images/cath.jpg"
+     },
+     
+     {
+      "flower" : "Lantana",
+      "price" : 3.00,
+      "image" : "./images/lantana.jpg"
+     },
+    
+     {
+      "flower" : "Marigold",
+      "price" : 5.00,
+      "image" : "./images/marigold.jpg"
+     },
+    
+     {
+      "flower" : "Petunia",
+      "price" : 8.00,
+      "image" : "./images/petunia.jpg"
+     },
+    
+     {
+      "flower" : "Rose",
+      "price" : 5.00,
+      "image" : "./images/rose.jpg"
+     }
+ ];
+ 
+ if(typeof module != 'undefined') {
+     module.exports = products;
+ }
