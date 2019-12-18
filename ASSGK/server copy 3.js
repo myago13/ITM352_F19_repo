1e1dbf522830619528025db5979c5a7031860c32
myago13 <54603677+myago13@users.noreply.github.com>
2019-12-17 20:29:49 -1000

    updated 12/17 8:30
    20:30 updated

--- ASSGK/server copy 3.js
+++ ASSGK/server copy 3.js
@@ -4,7 +4,6 @@ var express = require('express'); //initializes express to set up web server
 var myParser = require("body-parser"); //initializes body-parser to set up web server
 var filename = 'user_data.json' //Defines the user_data.json array as an object
 var app = express(); //Executes Express
-var qs = require('querystring'); //cannot change anything within the querstring
 
 
 fs = require('fs'); //Use the file system module 
@@ -92,72 +91,6 @@ app.post("/Total_ptpg.html", function (request, response) {
 //response.send(users_reg_data['itm352'].password); We only can retrieve the password from a specific user rather than all users
 // How to format using server????
 });
-
-
-    //*************************** REGISTER NEW USER *******************
-    app.post("/registration.html", function (request, response) {
-        // process a simple register form
-        username = request.body.username;//retrieves the username data
-        errors = {};//Checks to see if username already exists
-      
-        //Username Validation
-     if (typeof users_reg_data[username] != 'undefined'){
-     errors.username_error="Username is Already in Use"; //if username is in json file, say username is already in use
-     }
-     if ((/[a-z0-9]+/).test(request.body.username) ==false){ //only allows numbers and letters for the username
-        errors.username_error="Only numbers/letters";
-     }
-     if ((username.length > 10) ==true){
-        errors.username_error = "Please make your username shorter"; //if length is more than 10, show error to make the username shorter
-     }
-     if ((username.length < 4) ==true){
-        errors.username_error = "Please make your username longer"; //if length is less than 4, show error to make the username longer
-     }
-     
-     
-     
-     
-     //Fullname Validation // got help for the first fullname validation from Mr. Port
-     fullname = request.body.fullname;//retrieves the fullname data
-     if ((/[a-zA-Z]+[ ]+[a-zA-Z]+/).test(request.body.fullname) == false){
-     errors.fullname_error="Only use letters and a space";
-     }
-     
-     if ((fullname.length > 30) ==true){
-        errors.fullname_error = "Please make your full name shorter. 30 characters max"; //if length is greater than 30, send error that 30 characters are max
-     }
-     
-     
-     //Email Validation//
-     if ((/[a-z0-9._]+@[a-z0-9]+\.[a-z]+/).test(request.body.email) == false) {
-     errors.email_error="Please enter proper email";
-     }
-     
-     
-     
-     
-     console.log(errors, users_reg_data);
-     //If there are 0 errors, request all registration info
-     if (Object.keys(errors).length == 0){
-        users_reg_data[username] = {};
-        users_reg_data[username].username = request.body.username
-        users_reg_data[username].password = request.body.password;
-        users_reg_data[username].email = request.body.email;
-        users_reg_data[username].fullname = request.body.fullname;
-        users_reg_data[username].points = request.body.points;
-      
-     fs.writeFileSync(filename, JSON.stringify(users_reg_data)); //Writes registration info into the userdata json file
-     
-        response.redirect("/Total_ptpg.html?" + `&username=${username}`); //If all good, send to the total point page with username/quantity info
-     } else { 
-        qstring= qs.stringify(request.body)+"&"+qs.stringify(errors); 
-        response.redirect('/registration.html?' + qstring ); //if there are errors, send back to registration page to retype
-     }
-        
-     });
-
-
-
 
 
 
